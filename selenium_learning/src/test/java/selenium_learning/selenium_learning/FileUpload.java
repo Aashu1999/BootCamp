12df8fca2c97c28d9f15f8c26e0de64e49466bb8
Dell <Dell@DESKTOP-8VDRO24>
2020-09-17 23:14:26 +0530

    committed

---
+++ selenium_learning/src/test/java/selenium_learning/selenium_learning/FileUpload.java
@@ -0,0 +1,25 @@
+package selenium_learning.selenium_learning;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FileUpload {
+
+	public static void main(String[] args) {
+		 WebDriverManager.chromedriver().setup();
+	     WebDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		 
+		 driver.get("https://filebin.net/");
+		 driver.findElement(By.id("fileField")).sendKeys("C:\\Users\\Dell\\Desktop\\Sample.txt");;
+
+	}
+
+}
---
+++ selenium_learning/src/test/java/selenium_learning/selenium_learning/Locators.java
@@ -0,0 +1,27 @@
+package selenium_learning.selenium_learning;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locators {
+
+	public static void main(String[] args) {
+
+				 WebDriverManager.chromedriver().setup();
+			     WebDriver driver = new ChromeDriver();
+				 driver.manage().window().maximize();
+				 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+				 driver.get("https://opensource-demo.orangehrmlive.com/");
+				 driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+				 driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+				 driver.findElement(By.id("btnLogin")).click();
+
+	}
+
+}
